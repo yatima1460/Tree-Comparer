72434820b209638027ca33fc56b21bfd2738895b
Yatima Santamorena <yatima@santamorena.me>
2021-11-10 13:18:59 +0100

    add hash test and clean

--- tree-comparer.py
+++ tree-comparer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import hashlib
 import sys
 import getopt
 from os import listdir
@@ -13,60 +14,90 @@ print()
 logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
 
 
+BUF_SIZE = 65536  # 64kb chunks
+
+
+def hash(file):
+
+    sha1 = hashlib.sha1()
+
+    try:
+        listed_dir = listdir(reference_directory)
+        with open(file, 'rb') as f:
+            while True:
+                data = f.read(BUF_SIZE)
+                if not data:
+                    break
+                sha1.update(data)
+        return format(sha1.hexdigest())
+    except Exception as e:
+        logging.critical(e)
+        return None
+
 def get_parent(path: str) -> str:
     path = Path(path)
     return str(path.parent.absolute())
 
 
 def compare(reference_directory, test_directory):
-
-    logging.debug(f'Compare {reference_directory} => {test_directory}')
-
+    
     reference_directory_parent = get_parent(reference_directory)
     test_directory_parent = get_parent(test_directory)
-    
+
     listed_dir = None
-    
+
     try:
         listed_dir = listdir(reference_directory)
-    except PermissionError as e:
-        logging.error(f'Can\'t read directory {e}')
+    except Exception as e:
+        logging.critical(e)
         return
 
     for relative_file_path in listed_dir:
 
-
-
-        file_reference_directory = join(
-            reference_directory, relative_file_path)
+        relative_reference_file = join(reference_directory, relative_file_path)
         file_test_directory = join(test_directory, relative_file_path)
 
-        if os.path.isdir(file_reference_directory):
-            logging.debug(f'CheckDir {file_reference_directory} => {file_test_directory}')
-            if os.path.isdir(file_test_directory):
-                logging.debug(f'Directory {file_reference_directory} => {file_test_directory} exists')
-                compare(file_reference_directory, file_test_directory)
-            else:
-                logging.error(f'Directory "{file_reference_directory}" => does not exist in "{test_directory}"')
-        if os.path.isfile(file_reference_directory):
-            if os.path.isfile(file_test_directory):
-                logging.debug(f'File {file_reference_directory} => {file_test_directory} exists')
-                reference_size = os.path.getsize(file_reference_directory)
-                test_size = os.path.getsize(file_test_directory)
-                if test_size != reference_size:
-                    logging.warn(f'File size mismatch {file_reference_directory} ({reference_size} bytes) and {file_test_directory} ({test_size} bytes)')
-            else:
-                logging.error(f'File      "{file_reference_directory}" => does not exist in "{test_directory}"')
+        # If the reference file is a regular file
+        if os.path.isfile(relative_reference_file):
+
+            # Check if a regular file exists on other side as well
+            if not os.path.isfile(file_test_directory):
+                logging.error(f'File      "{relative_reference_file}" => does not exist in "{test_directory}"')
+                continue
+            
+            # Check if the sizes match
+            reference_size = os.path.getsize(relative_reference_file)
+            test_size = os.path.getsize(file_test_directory)
+            if test_size != reference_size:
+                logging.warn(f'File size mismatch {relative_reference_file} ({reference_size} bytes) and {file_test_directory} ({test_size} bytes)')
+                continue
+            
+            # Check if the hashes match
+            reference_hash = hash(relative_reference_file)
+            test_hash = hash(file_test_directory)
+            if reference_hash != test_hash:
+                logging.warn(f'File hash mismatch "{relative_reference_file}" ({reference_hash}) and "{file_test_directory}" ({test_hash})')
+                continue
+
+        # If the reference file is a directory
+        if os.path.isdir(relative_reference_file):
+
+            # Check if a directory exists on other side as well
+            if not os.path.isdir(file_test_directory):
+                logging.error(f'Directory "{relative_reference_file}" => does not exist in "{test_directory}"')
+                continue
+ 
+            compare(relative_reference_file, file_test_directory)
+            
 
 
 def main(argv):
     inputfile = argv[1]
     outputfile = argv[2]
-    logging.debug(f'Input dir is "{inputfile}" ')
-    logging.debug(f'Output dir is "{outputfile}" ')
+
     compare(inputfile, outputfile)
     compare(outputfile, inputfile)
 
 
 if __name__ == "__main__":
-   main(sys.argv)
+    main(sys.argv)
